90679f2e5fd404288f42c5a42887b4b2569d84b6
Chantal Taconet <chantal.taconet@telecom-sudparis.eu>
2026-08-04 16:19:14 +0200

    symbole mp3 plus gros

--- catalogue/outils/catalogue.py
+++ catalogue/outils/catalogue.py
@@ -19,7 +19,7 @@ def partitionGraveeURL(u):
 def partitionManuscriteURL(u):
     return f'<a href="../partitions/Manuscrit/{u}" target="_blank">PDF</a>'
 def extraitURL(u):
-    return f'<a href="../extraits/{u}" target="_blank">&#9835;</a>'
+    return f'<a href="../extraits/{u}" target="_blank"><b> <font size="+4">&#9836;</font></b></a>'
 def poemeURL(u):
     return f'<a href="../{u}" target="_blank">poème</a>'
    
@@ -46,7 +46,7 @@ def lire_formater_catalogue():
 def filtrer_catalogue(df, filtre):
   match filtre:
     case "voix":
-      df = df[df["Genre"].str.contains("Mélodie|Mélodie|Ensemble vocal",case=False,na=False)]
+      df = df[df["Genre"].str.contains("Mélodie|Religieuse|Ensemble vocal",case=False,na=False)]
       df = df.drop(columns=["Genre"])
       return df
     case "mélodie":
@@ -101,45 +101,25 @@ def filtrer_catalogue(df, filtre):
     # paging=False
 def afficher_catalogue(df):
   #df = df.reset_index(drop=True)
-  html = """
-  <div class="catalogue-wrapper">
-  """
-  display(HTML(html))
 
   show(df, searchable=True, sortable=True, allow_html=True,columnControl=["order", "colVisDropdown", "searchDropdown"])
   html = """
 <p><br><br>* Les partitions manuscrites ne portent pas de date, les dates mentionnées sont soit celles de dépôt à la SACEM, 
 pour les oeuvres déposées à la SACEM, soit viennent de la mention de l'oeuvre dans une correspondance.</p>
 
-<p>J'ai rassemblé ici les poèmes mis en musique par Georges TACONET. 
-Ils sont présentés dans l'ordre du catalogue, c'est à dire suivant l'ordre alphabétique des poè­tes. 
-Il eut été intéressant de connaître la date de composi­tion de chacun d'eux, 
-ne serait-ce que pour voir l'évolution dans le temps de l'inspiration du musicien. 
-Nous n'avons pas ces dates, mais nous avons quelques indications sur certaines œuvres, 
-en tenant compte de la date d'une pre­mière audition, de la date d'inscription à la SACEM, 
-ou même de l'écriture du manuscrit.
-
-On peut noter que le jeune compositeur a pris quelques (rares) libertés avec le texte des poésies 
-qu’il mettait en musique (voir annotations en bas de page).
-</p>
-<p>
-Signalons que certains de ces poèmes ont inspiré d’autres musiciens :
-<ul>
-<li>Gabriel Fauré (Soir, d’Albert Samain ; Au bord de l’eau, de Sully Prud’homme ; Tous deux, de Verlaine)</li>
-<li>E. Chausson (Dans la forêt chauve et rouillée, de Théophile Gautier)</li>
-<li>Cl. Debussy (Rondel, de Charles d’Orléans)</li>
-</ul>
-Je me fais un devoir et un plaisir de remercier ici Mme Mauricette Vinay. 
-Fille de Blanche Vinay-Leconte, violoniste de grand talent, très appréciée de Georges Taconet, 
-et elle même agrégée de lettres, elle m’a signalé et évité bien des fautes de frappe ou de prosodie. 
-En outre elle a rédigé l’essentiel des deux pages qui suivent : Situons les Poètes.
-</p>
 <p>
 Henri Taconet
 </br>
 avril 1998-juin 200
 </p>
-</div>
+<p><i>J'ai rassemblé ici les poèmes mis en musique par Georges TACONET. 
+Ils sont présentés dans l'ordre du catalogue, c'est à dire suivant l'ordre alphabétique des poè­tes. 
+Il eut été intéressant de connaître la date de composi­tion de chacun d'eux, 
+ne serait-ce que pour voir l'évolution dans le temps de l'inspiration du musicien. 
+Nous n'avons pas ces dates, mais nous avons quelques indications sur certaines œuvres, 
+en tenant compte de la date d'une pre­mière audition, de la date d'inscription à la SACEM, 
+ou même de l'écriture du manuscrit.</i>
+</p>
 """
 
   display(HTML(html))
